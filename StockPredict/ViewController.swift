dc6841fea95bf5bb0dd1cf610ea231fe2064aab6
Peter-Chong <wlchong@edu.uwaterloo.ca>
2020-01-20 15:31:35 -0500

    Complete twitter api

--- StockPredict/ViewController.swift
+++ StockPredict/ViewController.swift
@@ -7,15 +7,28 @@
 //
 
 import UIKit
+import SwifteriOS
+import CoreML
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var sentimentLabel: UILabel!
     @IBOutlet weak var textField: UITextField!
     
+    let swifter = Swifter(consumerKey: "iEz6tiaMkhI5fLFhyCJfmJqwG", consumerSecret: "RhaIoLJRlCs8mAynAfsgxlf8dnoXN5jLuNd2mNiXm9Ju406yem")
+    
+    let sentimentClassifier = TweetSentimentClassifier()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        swifter.searchTweet(using: <#T##String#>,lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
+            print(results)
+        }) { (error) in
+            print("There was an error with API request, \(error)")
+        }
+        
     }
 
     @IBAction func predictPressed(_ sender: UIButton) {
