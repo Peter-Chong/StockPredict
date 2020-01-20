9c01c936ce807474e2e2318f6c6866374a649d18
Peter-Chong <wlchong@edu.uwaterloo.ca>
2020-01-20 13:53:55 -0500

    Initial Commit

---
+++ StockPredict/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StockPredict
+//
+//  Created by Weng Liang Chong on 20/01/2020.
+//  Copyright © 2020 Weng Liang Chong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
